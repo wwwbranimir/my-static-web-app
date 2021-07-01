2913c6684a3527bc5a01e8dcb406227ff821223f
Branimir B <branimir.bzenic@euromonitor.com>
2021-07-01 20:43:34 +0200

    new commit

--- angular-app/src/app/core/components/header-bar-brand.component.ts
+++ angular-app/src/app/core/components/header-bar-brand.component.ts
@@ -13,7 +13,7 @@ import { Component } from '@angular/core';
         <i class="fab js-logo fa-angular fa-2x" aria-hidden="true"></i>
       </a>
       <a class="navbar-item nav-home" router-link="/">
-        <span class="brand-first">MY</span>
+        <span class="brand-first">BANE</span>
         <span class="brand-second">SHOPPING</span>
         <span class="brand-third">LIST</span>
       </a>
